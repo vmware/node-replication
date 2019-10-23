1ba95d23d87f78f682c99ebecc781afe5658064c
Gerd Zellweger <mail@gerdzellweger.com>
2019-10-23 14:14:40 -0700

    Fix hashbench script.

--- benches/hashbench_run.sh
+++ benches/hashbench_run.sh
@@ -7,7 +7,7 @@ for w in 1 4 8 16 24; do
   for d in uniform skewed; do
     for r in 1 4 8 16 24; do
       if [[ $w > 0 ]] || [[ $r > 0 ]] ; then
-        RUST_TEST_THREADS=1 cargo bench --bench hashbench -- -r $r -w $w -d $d -c | tee -a results.log;
+        RUST_TEST_THREADS=1 cargo bench --bench hashbench -- -r $r -w $w -d $d | tee -a results.log;
       fi
     done;
   done;
