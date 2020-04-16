0ef9e62d3c5cc83bec448348914890ebd02a4322
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2020-04-16 17:33:41 -0400

    Avoid running hashmap benchmark with interleaved mode

--- benches/hashmap.rs
+++ benches/hashmap.rs
@@ -16,7 +16,6 @@ mod mkbench;
 mod utils;
 
 use utils::benchmark::*;
-use utils::topology::ThreadMapping;
 use utils::Operation;
 
 extern crate jemallocator;
@@ -166,7 +165,6 @@ fn hashmap_scale_out(c: &mut TestHarness, write_ratio: usize) {
     mkbench::ScaleBenchBuilder::<NrHashMap>::new(ops)
         .machine_defaults()
         .add_batch(128)
-        .thread_mapping(ThreadMapping::Interleave)
         .configure(
             c,
             &bench_name,
@@ -196,7 +194,6 @@ fn partitioned_hashmap_scale_out(c: &mut TestHarness, write_ratio: usize) {
     mkbench::ScaleBenchBuilder::<NrHashMap>::new(ops)
         .machine_defaults()
         .add_batch(128)
-        .thread_mapping(ThreadMapping::Interleave)
         .update_replica_strategy(mkbench::ReplicaStrategy::Partition)
         .configure(
             c,
