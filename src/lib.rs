b8ce427c15e5b6909dc540424471cbbfd5e62f06
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2020-06-01 19:37:45 -0400

    Add hashmap example for rust docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,62 @@
 // Copyright © 2019 VMware, Inc. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! An operation-log based approach for data replication.
+//! Node Replication(NR) is a library which can be used to implement a concurrent version of any
+//! single threaded data structure: It takes in a single threaded implementation of said data
+//! structure, and scales it out to multiple cores and NUMA nodes by combining two techniques:
+//! operation logging and flat combining.
+//!
+//! # How does it work
+//! To replicate a single-threaded data structure, one needs to implement [Dispatch](trait.Dispatch.html) trait.
+//! The following example uses hashmap as an example.
+//! ```
+//! use node_replication::Dispatch;
+//! use std::collections::HashMap;
+//!
+//! /// The single-threaded NrHashMap which internally uses std HashMap.
+//! pub struct NrHashMap {
+//!    storage: HashMap<u64, u64>,
+//! }
+//!
+//! /// We support mutable Put() operation on the NrHashMap.
+//! #[derive(Debug, Eq, PartialEq, Clone)]
+//! pub enum OpWr {
+//!    Put(u64, u64),
+//! }
+//!
+//! /// We support immutable Get() operation on the NrHashMap.
+//! #[derive(Debug, Eq, PartialEq, Clone)]
+//! pub enum OpRd {
+//!    Get(u64),
+//! }
+//!
+//! /// The Dispatch traits executes `ReadOperation` (our OpRd enum)
+//! /// and `WriteOperation` (our `OpWr` enum) against the replicated
+//! /// data-structure.
+//! impl Dispatch for NrHashMap {
+//!    type ReadOperation = OpRd;
+//!    type WriteOperation = OpWr;
+//!    type Response = Option<u64>;
+//!    type ResponseError = ();
+//!
+//!    /// The `dispatch` function applies the immutable operations.
+//!    fn dispatch(&self, op: Self::ReadOperation) -> Result<Self::Response, Self::ResponseError> {
+//!        match op {
+//!            OpRd::Get(key) => Ok(self.storage.get(&key).map(|v| *v)),
+//!        }
+//!    }
+//!
+//!    /// The `dispatch_mut` function applies the mutable operations.
+//!    fn dispatch_mut(
+//!        &mut self,
+//!        op: Self::WriteOperation,
+//!    ) -> Result<Self::Response, Self::ResponseError> {
+//!        match op {
+//!            OpWr::Put(key, value) => Ok(self.storage.insert(key, value)),
+//!        }
+//!    }
+//! }
+//! ```
 #![no_std]
 #![feature(atomic_min_max)]
 #![feature(new_uninit)]
