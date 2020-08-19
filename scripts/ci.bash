2fe3f1d096e5fefbf8ee1fde81fc170e7c257678
Gerd Zellweger <mail@gerdzellweger.com>
2020-08-18 18:47:44 -0700

    Point to master branch

--- scripts/ci.bash
+++ scripts/ci.bash
@@ -23,7 +23,7 @@ RUST_TEST_THREADS=1 timeout 20h cargo bench --bench lockfree
 
 # Check that we can checkout gh-pages early:
 rm -rf gh-pages
-git clone --depth 1 -b gh-pages git@github.com:gz/nr-benchmarks.git gh-pages
+git clone --depth 1 -b master git@github.com:gz/nr-benchmarks.git gh-pages
 pip3 install -r gh-pages/requirements.txt
 
 # Copy scalebench
@@ -68,7 +68,7 @@ python3 history_plots.py
 # Push to gh-pages
 git add .
 git commit -a -m "Added benchmark results for $GIT_REV_CURRENT."
-git push origin gh-pages
+git push origin master
 cd ..
 
 rm -rf gh-pages
