9787aeecea11d064f31aea8807c47e08941d69d7
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2021-05-04 23:41:21 -0400

    Revert "Temp: Later revert."
    This reverts commit ba7ea06431d86a7e6dfab50ca44e6f5fb6994ea3.

--- benches/lockfree_partitioned.rs
+++ benches/lockfree_partitioned.rs
@@ -91,7 +91,6 @@ impl Default for SkipListWrapper {
         for i in 0..INITIAL_CAPACITY {
             storage.insert(i as u64, i as u64);
         }
-        log::error!("Default");
         SkipListWrapper(storage)
     }
 }
