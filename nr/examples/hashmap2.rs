65f4fe9456859da733ff1b98bbf472f72be1e532
Gerd Zellweger <mail@gerdzellweger.com>
2022-02-15 13:18:43 -0800

    Fix return value.

--- nr/examples/hashmap2.rs
+++ nr/examples/hashmap2.rs
@@ -56,54 +56,57 @@ impl Dispatch for NrHashMap {
 fn main() {
     let _r = env_logger::try_init();
     // Next, we create two replicas of the hashmap
-    let num_replica = NonZeroUsize::new(4).unwrap();
-    let nrht = Arc::new(NodeReplicated::<NrHashMap>::new(num_replica, |_rid| {}).unwrap());
 
-    // The replica executes a Modify or Access operations by calling
-    // `execute_mut` and `execute`. Eventually they end up in the `Dispatch` trait.
-    let thread_loop = |replica: Arc<NodeReplicated<NrHashMap>>, ttkn| {
-        for i in 0..2_948_048 {
-            let _r = match i % 2 {
-                0 => replica.execute_mut(Modify::Put(i, i + 1), ttkn),
-                1 => {
-                    let response = replica.execute(Access::Get(i - 1), ttkn);
-                    assert_eq!(response, Some(i));
-                    response
-                }
-                _ => unreachable!(),
-            };
+    for i in 4..=4 {
+        let num_replica = NonZeroUsize::new(4).unwrap();
+        let nrht = Arc::new(NodeReplicated::<NrHashMap>::new(num_replica, |_rid| {}).unwrap());
+
+        // The replica executes a Modify or Access operations by calling
+        // `execute_mut` and `execute`. Eventually they end up in the `Dispatch` trait.
+        let thread_loop = |replica: Arc<NodeReplicated<NrHashMap>>, ttkn| {
+            for i in 0..2_948_048 {
+                let _r = match i % 2 {
+                    0 => replica.execute_mut(Modify::Put(i, i + 1), ttkn),
+                    1 => {
+                        let response = replica.execute(Access::Get(i - 1), ttkn);
+                        assert_eq!(response, Some(i));
+                        response
+                    }
+                    _ => unreachable!(),
+                };
+            }
+        };
+
+        // Finally, we spawn three threads that issue operations, thread 1, 2, 3
+        // will use replicas 1, 2, 3 -- 4th replica will implicitly be served by others
+        // because we can in this model...
+        let mut threads = Vec::with_capacity(4);
+        let nrht_cln = nrht.clone();
+        threads.push(std::thread::spawn(move || {
+            let ttkn = nrht_cln.register(0).expect("Unable to register thread");
+            thread_loop(nrht_cln, ttkn);
+        }));
+        let nrht_cln = nrht.clone();
+        threads.push(std::thread::spawn(move || {
+            let ttkn = nrht_cln.register(0).expect("Unable to register thread");
+            thread_loop(nrht_cln, ttkn);
+        }));
+
+        /*let nrht_cln = nrht.clone();
+        threads.push(std::thread::spawn(move || {
+            let ttkn = nrht_cln.register(1).expect("Unable to register thread");
+            thread_loop(nrht_cln, ttkn);
+        }));
+
+        let nrht_cln = nrht.clone();
+        threads.push(std::thread::spawn(move || {
+            let ttkn = nrht_cln.register(2).expect("Unable to register thread");
+            thread_loop(nrht_cln, ttkn);
+        }));*/
+
+        // Wait for all the threads to finish
+        for thread in threads {
+            thread.join().unwrap();
         }
-    };
-
-    // Finally, we spawn three threads that issue operations, thread 1, 2, 3
-    // will use replicas 1, 2, 3 -- 4th replica will implicitly be served by others
-    // because we can in this model...
-    let mut threads = Vec::with_capacity(4);
-    let nrht_cln = nrht.clone();
-    threads.push(std::thread::spawn(move || {
-        let ttkn = nrht_cln.register(0).expect("Unable to register thread");
-        thread_loop(nrht_cln, ttkn);
-    }));
-    let nrht_cln = nrht.clone();
-    threads.push(std::thread::spawn(move || {
-        let ttkn = nrht_cln.register(0).expect("Unable to register thread");
-        thread_loop(nrht_cln, ttkn);
-    }));
-
-    let nrht_cln = nrht.clone();
-    threads.push(std::thread::spawn(move || {
-        let ttkn = nrht_cln.register(1).expect("Unable to register thread");
-        thread_loop(nrht_cln, ttkn);
-    }));
-
-    let nrht_cln = nrht.clone();
-    threads.push(std::thread::spawn(move || {
-        let ttkn = nrht_cln.register(2).expect("Unable to register thread");
-        thread_loop(nrht_cln, ttkn);
-    }));
-
-    // Wait for all the threads to finish
-    for thread in threads {
-        thread.join().unwrap();
     }
 }
