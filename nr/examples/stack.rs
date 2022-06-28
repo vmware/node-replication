5c6436bc8a30b7a6f798c06fdb9e0cda1e55624a
Gerd Zellweger <mail@gerdzellweger.com>
2022-06-27 22:52:22 -0700

    Make stack example generic.

--- nr/examples/stack.rs
+++ nr/examples/stack.rs
@@ -1,21 +1,24 @@
 // Copyright © 2019-2020 VMware, Inc. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! A minimal example that implements a node-replicated stack
+//! A minimal example that implements a node-replicated, generic stack
 
 #![feature(const_option)]
 
+use std::cmp::PartialEq;
 use std::num::NonZeroUsize;
+use std::ops::Deref;
 use std::sync::Arc;
 
 use node_replication::Dispatch;
 use node_replication::NodeReplicated;
 use node_replication::NodeReplicatedError;
+use node_replication::ThreadToken;
 
 /// We support mutable push and pop operations on the stack.
 #[derive(Clone, Debug, PartialEq)]
-enum Modify {
-    Push(u32),
+enum Modify<T> {
+    Push(T),
     Pop,
 }
 
@@ -26,24 +29,30 @@ enum Access {
 }
 
 /// The actual stack is implemented with a (single-threaded) Vec.
-struct Stack {
-    storage: Vec<u32>,
+struct Stack<T>
+where
+    T: Default,
+{
+    storage: Vec<T>,
 }
 
-impl Default for Stack {
+impl<T> Default for Stack<T>
+where
+    T: Default,
+{
     /// The stack Default implementation.
     ///
     /// This should be deterministic as it is used to create multiple instances of a Stack
     /// for every replica during initialization.
-    fn default() -> Stack {
+    fn default() -> Self {
         const DEFAULT_STACK_SIZE: u32 = 1_000u32;
 
         let mut s = Stack {
             storage: Default::default(),
         };
 
-        for e in 0..DEFAULT_STACK_SIZE {
-            s.storage.push(e);
+        for _i in 0..DEFAULT_STACK_SIZE {
+            s.storage.push(T::default());
         }
 
         s
@@ -52,10 +61,13 @@ impl Default for Stack {
 
 /// The Dispatch traits executes `ReadOperation` (our Access enum) and `WriteOperation`
 /// (our `Modify` enum) against the replicated data-structure.
-impl Dispatch for Stack {
+impl<T> Dispatch for Stack<T>
+where
+    T: Default + Clone + PartialEq + Send,
+{
     type ReadOperation = Access;
-    type WriteOperation = Modify;
-    type Response = Option<u32>;
+    type WriteOperation = Modify<T>;
+    type Response = Option<T>;
 
     /// The `dispatch` function applies the immutable operations.
     fn dispatch(&self, op: Self::ReadOperation) -> Self::Response {
@@ -76,6 +88,38 @@ impl Dispatch for Stack {
     }
 }
 
+struct NrStack<T>(Arc<NodeReplicated<Stack<T>>>, ThreadToken)
+where
+    T: Sync + Default + Clone + PartialEq + Send;
+
+impl<T> Deref for NrStack<T>
+where
+    T: Sync + Default + Clone + PartialEq + Send,
+{
+    type Target = NodeReplicated<Stack<T>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> NrStack<T>
+where
+    T: Sync + Default + Clone + PartialEq + Send,
+{
+    fn push(&self, v: T) {
+        self.execute_mut(Modify::Push(v), self.1);
+    }
+
+    fn pop(&self) -> Option<T> {
+        self.execute_mut(Modify::Pop, self.1)
+    }
+
+    fn peek(&self) -> Option<T> {
+        self.execute(Access::Peek, self.1)
+    }
+}
+
 /// We initialize a replicated stack with two replicas, then spawn threads, assign them to
 /// replicas and execute operations.
 fn main() -> Result<(), NodeReplicatedError> {
@@ -87,12 +131,15 @@ fn main() -> Result<(), NodeReplicatedError> {
 
     // The replica executes a Modify or Access operations by calling
     // `execute_mut` and `execute`. Eventually they end up in the `Dispatch` trait.
-    let thread_loop = |stack: &Arc<NodeReplicated<Stack>>, ridx| {
+    let thread_loop = |stack: NrStack<usize>| {
         for i in 0..NUM_OPS_PER_THREAD {
-            let _r = match i % 3 {
-                0 => stack.execute_mut(Modify::Push(i as u32), ridx),
-                1 => stack.execute_mut(Modify::Pop, ridx),
-                2 => stack.execute(Access::Peek, ridx),
+            match i % 3 {
+                0 => {
+                    stack.push(i);
+                    Some(i)
+                }
+                1 => stack.pop(),
+                2 => stack.peek(),
                 _ => unreachable!(),
             };
         }
@@ -105,7 +152,7 @@ fn main() -> Result<(), NodeReplicatedError> {
             let ridx = stack
                 .register(idx % NUM_REPLICAS)
                 .expect("Unable to register with stack");
-            thread_loop(&stack, ridx);
+            thread_loop(NrStack(stack, ridx));
         }));
     }
 
