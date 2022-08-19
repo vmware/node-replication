907c3f8f21749506645838dfc2c1b94546801b56
Gerd Zellweger <mail@gerdzellweger.com>
2022-08-19 16:37:53 -0700

    Flesh out benchmark more.

--- node-replication/examples/simdr.rs
+++ node-replication/examples/simdr.rs
@@ -6,27 +6,50 @@
 
 use std::arch::x86_64::*;
 use std::simd::Simd;
+use std::thread;
 
 use memmap::MmapMut;
 use memmap::MmapOptions;
-use std::fs::File;
-use std::io::Write;
 
 fn make_regions(region_count: usize, region_size: usize) -> Vec<MmapMut> {
     let mut regions = Vec::with_capacity(region_count);
-    for r in 0..region_count {
+    for _r in 0..region_count {
         regions.push(MmapOptions::new().len(region_size).map_anon().unwrap());
     }
 
     regions
 }
 
-fn main() {
-    unsafe {
-        let region_size: usize = 1024 * 1024 * 4;
-        let regions_cnt = 4;
+fn threadsline(regions: &mut Vec<MmapMut>, region_size: usize, input: &Vec<u8>) {
+    assert!(input.len() <= region_size);
+
+    thread::scope(|s| {
+        let mut threads = Vec::with_capacity(regions.len());
         
-        let regions = make_regions(regions_cnt, region_size);
+        for region in regions {
+            threads.push(s.spawn(move || {
+                let ptr = region.as_mut();
+                ptr[0..input.len()].copy_from_slice(input.as_slice());
+            }));
+        }
+
+        for thread in threads {
+            thread.join().expect("Can't join thread?");
+        }
+    });
+}
+
+fn baseline(regions: &mut Vec<MmapMut>, region_size: usize, input: &Vec<u8>) {
+    assert!(input.len() <= region_size);
+
+    for region in regions {
+        let ptr = region.as_mut();
+        ptr[0..input.len()].copy_from_slice(input.as_slice());
+    }
+}
+
+fn simdline(regions: &mut Vec<MmapMut>, _region_size: usize, _input: &Vec<u8>) {
+    unsafe {
         let region_base_addrs = regions.iter().map(|r| r.as_ref().as_ptr() as i64);
 
         let mut vindex_slice = [0; 8];
@@ -37,12 +60,12 @@ fn main() {
 
         let base_address: *mut u8 = std::ptr::null_mut();
 
-        assert!(regions_cnt.is_power_of_two());
-        assert!(regions_cnt <= u8::MAX.into());
+        assert!(regions.len().is_power_of_two());
+        assert!(regions.len() <= u8::MAX.into());
         fn setbits(x: u8) -> u8 {
             u8::MAX >> (8 - x)
         }
-        let k: __mmask8 = setbits(regions_cnt as u8);
+        let k: __mmask8 = setbits(regions.len() as u8);
 
         let vindex = Simd::<i64, 8>::from_array(vindex_slice);
         let a: __m512d = Simd::<f64, 8>::from_array([
@@ -58,8 +81,8 @@ fn main() {
         .into();
         const SCALE: i32 = 1;
 
-        assert!(regions_cnt <= 8);
-        let r = _mm512_mask_i64scatter_pd(base_address, k, vindex.into(), a, SCALE);
+        assert!(regions.len() <= 8);
+        _mm512_mask_i64scatter_pd(base_address, k, vindex.into(), a, SCALE);
 
         for region in regions.iter() {
             assert_eq!(
@@ -68,4 +91,18 @@ fn main() {
             );
         }
     }
+}
+
+fn main() {
+    let region_size: usize = 1024 * 1024 * 4;
+    let regions_cnt = 4;
+    let mut regions = make_regions(regions_cnt, region_size);
+
+    let mut to_write: Vec<u8> = Vec::with_capacity(4096);
+    to_write.extend_from_slice(&[0xffu8; 4096]);
+    assert_eq!(to_write.len(), 4096);
+
+    baseline(&mut regions, region_size, &to_write);
+    threadsline(&mut regions, region_size, &to_write);
+    simdline(&mut regions, region_size, &to_write);
 }
